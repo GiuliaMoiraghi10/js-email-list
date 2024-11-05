2af43600f30f9dac5e9107f86a8b8a92a3c92038
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-11-05 15:08:50 +0100

    Elenco passaggi da fare

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('JS-EMAIL-LIST')
+
+// Descrizione
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+// Bonus
+// Abbellire con CSS o Bootstrap
+// Inserire un bottone che al click faccia il fetch di altre 10 mail (sostituendo le altre)
+
+//Passaggi:
+// 1) Creo axios con url api https://flynn.boolean.careers/exercises/api/random/mail
+// 2) Aggiungo Query String
+// 3) Stampo email generate random in array
+// 4) Ciclo array per avere email stampate singolarmente
+// 5) Pusho array dentro una lista creata con html
+// 6) Creo bottone html
+// 7) Aggiungo evento al click del bottone che mi fa vedere in pagina la lista di email
+
